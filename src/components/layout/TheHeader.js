cd53f41cfd5afd37192e82161924e3e7b2573ce0
juliadud <yuliiadudkina@gmail.com>
2021-11-21 11:26:53 +0200

    adding header

---
+++ src/components/layout/TheHeader.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TheHeader extends Component {
+    render() {
+        return (
+            <div className={"header"}>
+                <h1>Futurama</h1>
+            </div>
+        );
+    }
+}
+
+export default TheHeader;
