26ce477a426646829c3dec7beee2b4f532988436
juliadud <yuliiadudkina@gmail.com>
2021-11-25 10:16:35 +0200

    adding router

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter,  Routes, Route } from "react-router-dom";
+import Quotes from "./components/Quotes";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="quotes" element={<Quotes />} />
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
